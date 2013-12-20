251976c0618aefffff93ec561be585c104cb258c
Nailton Sousa <nailton@totalinterativa.com.br>
2013-12-20 09:47:59 -0200

    UPDATE - Instala app colocado como auth allow

--- app/Plugin/Instala/Controller/InstalaAppController.php
+++ app/Plugin/Instala/Controller/InstalaAppController.php
@@ -1,7 +1,7 @@
 <?php
 class InstalaAppController extends AppController {
 	function beforeFilter(){
-		// $this->Auth->allow();
-		$this->set('user',false);
+		$this->Auth->allow();
+		// $this->set('user',false);
 	}
 }
